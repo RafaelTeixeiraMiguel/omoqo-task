2a1274fcff1845b18c4461da51d254e52865dda7
Rafael Teixeira Miguel <teixeirarafael12@gmail.com>
2023-07-15 00:34:22 -0300

    Created the first basic controller for Ships management

---
+++ api/Omoqo.Task/API/Controllers/ShipsController.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ShipsController : BaseController
+    {
+        private readonly DataContext _context;
+
+        public ShipsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Ship>>> GetShips()
+        {
+            return await _context.Ships.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ship>> GetShip(Guid id)
+        {
+            return await _context.Ships.FindAsync(id);
+        }
+    }
+}
