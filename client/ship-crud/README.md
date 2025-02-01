70a98c43eb192f176b6f2846e513d347aafd3d06
RafaelTeixeiraMiguel <teixeirarafael12@gmail.com>
2025-02-01 16:02:05 -0300

    Changing name

--- client/ship-crud/README.md
+++ client/ship-crud/README.md
@@ -1,4 +1,4 @@
-# OmoqoTask
+# SHIP Crud
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.4.
 
--- client/ship-crud/src/app/app.component.spec.ts
+++ client/ship-crud/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'omoqo-task'`, () => {
+  it(`should have as title 'ship-crud'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('omoqo-task');
+    expect(app.title).toEqual('ship-crud');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('omoqo-task app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ship-crud app is running!');
   });
 });
